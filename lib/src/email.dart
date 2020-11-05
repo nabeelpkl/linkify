538e40b8c5af938c171d6610f4ec8fb36a083067
Nabeel K <nabeel.kottol@gmail.com>
2020-11-05 17:16:59 +0530

    accept more characters

--- lib/src/email.dart
+++ lib/src/email.dart
@@ -1,7 +1,7 @@
 import 'package:linkify/linkify.dart';
 
 final _emailRegex = RegExp(
-  r'^((?:.|\n)*?)((mailto:)?[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4})',
+  r'^((?:.|\n)*?)((mailto:)?[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,10})',
   caseSensitive: false,
 );
 
